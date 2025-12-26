aed933708312c370e40e46dc1f19b8c2a46028eb
Omar <omar.garcia@morsa.com.mx>
2025-12-26 13:00:58 -0600

    Diferentes METODOS HTTP

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,26 @@ const server = express();
 
 // Routing - req: es lo que recibe el servidor o lo que tu estas enviando, res: es lo que el servidor responde
 server.get('/', (req, res) => {
-  res.send('Hello, World!');
-  res.json('Hello, World!');
+  // res.send('Hello, World!');
+  // res.json('Hello, World!');
+
+  res.json('Desde GET');
+});
+
+server.post("/", (req, res) => {
+  res.json("Desde POST");
+});
+
+server.put("/", (req, res) => {
+  res.json("Desde PUT");
+});
+
+server.patch("/", (req, res) => {
+  res.json("Desde PATCH");
+});
+
+server.delete("/", (req, res) => {
+  res.json("Desde DELETE");
 });
 
 export default server;

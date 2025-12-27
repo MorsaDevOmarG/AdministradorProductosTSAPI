5b9231d62e24b377def0a500f2d2d9612e400f2c
Omar <omar.garcia@morsa.com.mx>
2025-12-27 00:07:01 -0600

    Agregando PRODUCTOS a la BD, correccion

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -5,10 +5,12 @@ import Product from "../models/Product.model";
 export const createProduct = async (req : Request, res : Response) => {
   // console.log(colors.yellow(req.body));
 
-  const product = new Product(req.body);
-  const savedProduct = await product.save();
+  // const product = new Product(req.body);
+  // const savedProduct = await product.save();
 
-  console.log(colors.magenta(`Producto creado: ${JSON.stringify(savedProduct)}`));
+  // console.log(colors.magenta(`Producto creado: ${JSON.stringify(savedProduct)}`));
+
+  const product = await Product.create(req.body);
 
   // Almacenar en la BD
   // await product.save();

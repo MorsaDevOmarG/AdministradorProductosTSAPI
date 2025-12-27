438814ba6331e7b02088357e182cb60091586fe0
Omar <omar.garcia@morsa.com.mx>
2025-12-26 22:15:53 -0600

    SEparando las RUTAS en su propio archivo

---
+++ src/router.ts
@@ -0,0 +1,29 @@
+import { Router } from "express";
+
+const router = Router();
+
+// Routing - req: es lo que recibe el servidor o lo que tu estas enviando, res: es lo que el servidor responde
+router.get("/", (req, res) => {
+  // res.send('Hello, World!');
+  // res.json('Hello, World!');
+
+  res.json("Desde GET");
+});
+
+router.post("/", (req, res) => {
+  res.json("Desde POST");
+});
+
+router.put("/", (req, res) => {
+  res.json("Desde PUT");
+});
+
+router.patch("/", (req, res) => {
+  res.json("Desde PATCH");
+});
+
+router.delete("/", (req, res) => {
+  res.json("Desde DELETE");
+});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,8 @@
 import express from 'express';
+import router from './router';
 
 const server = express();
 
-// Routing - req: es lo que recibe el servidor o lo que tu estas enviando, res: es lo que el servidor responde
-server.get('/', (req, res) => {
-  // res.send('Hello, World!');
-  // res.json('Hello, World!');
-
-  res.json('Desde GET');
-});
-
-server.post("/", (req, res) => {
-  res.json("Desde POST");
-});
-
-server.put("/", (req, res) => {
-  res.json("Desde PUT");
-});
-
-server.patch("/", (req, res) => {
-  res.json("Desde PATCH");
-});
-
-server.delete("/", (req, res) => {
-  res.json("Desde DELETE");
-});
+server.use('/api', router);
 
 export default server;

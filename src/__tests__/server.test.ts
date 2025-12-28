c8e85d1cb9ff1fb59574e3fec83ce142d2a46491
Omar <omar.garcia@morsa.com.mx>
2025-12-28 09:11:21 -0600

    Negando una condicion para evitar falsos positivos

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -16,14 +16,15 @@ describe('GET /api', () => {
     const response = await request(server).get('/api');
     expect(response.status).toBe(200);
     expect(response.header['content-type']).toMatch(/json/);
-    // expect(response.body).toEqual({ msg: 'API de Productos funcionando' });
     expect(response.body.msg).toBe('API de Productos funcionando');
+    expect(response.status).not.toBe(404);
+    expect(response.body.msg).not.toBe('api de productos funcionando');
 
     // console.log(response);
     // console.log(response.status);
     // console.log(response.header['content-type']);
-    console.log(response.text);
-    console.log(response.body);
-    console.log(response.body.msg);
+    // console.log(response.text);
+    // console.log(response.body);
+    // console.log(response.body.msg);
   });
 });
